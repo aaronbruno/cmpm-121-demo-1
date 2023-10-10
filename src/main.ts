e4768b95f140f9168b6fad36679bf6085a6c58e0
Aaron <apbruno@ucsc.edu>
2023-10-10 15:46:58 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,27 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Aaron's game";
+const gameName = "Aaron's Test Game";
+
+const emojiButton = "🏋️";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
+const button = document.createElement("button");
+button.textContent = emojiButton;
+
 app.append(header);
+app.append(button);
+
+// Webpage alert to show that button was clicked
+function handleClick() {
+  alert("Button clicked!");
+}
+
+// Listens if button is clicked, and if it is run function
+if (button) {
+  button.addEventListener("click", handleClick);
+}
