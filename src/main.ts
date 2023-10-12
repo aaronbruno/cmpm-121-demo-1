4fa53bbfc164916745aefa6a1c0da85b17df58a2
Aaron <apbruno@ucsc.edu>
2023-10-11 23:20:28 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Muscle Clicker";
 const emojiButton = "💪";
+const emojiButton2 = "🏋🏻‍♂️"; // 🎧
 document.title = "Aaron's Game: " + gameName;
 
 // Header
@@ -15,49 +16,80 @@ const button = document.createElement("button");
 button.textContent = emojiButton;
 button.style.fontSize = "60px";
 
+// Side Button for auto upgrades (for auto counting)
+const sideButton = document.createElement("button");
+sideButton.textContent = emojiButton2;
+sideButton.style.fontSize = "40px";
+
 // Counter for clicking
 let totalCount: number = 0;
 const counterText = document.createElement("div");
 counterText.style.fontSize = "25px";
 
+// # to increment per second
+let incrementPerSecond: number = 1;
+
 // Click to increase
 const clickIncrease: number = 1;
 button.addEventListener("click", () => {
   updateCounter(counterText, clickIncrease);
+  // updateCounter(counterText, totalIncrement);
+});
+
+const upgradeCost: number = -10;
+// Click to buy upgrade
+sideButton.addEventListener("click", () => {
+  updateCounter(counterText, upgradeCost);
+  updateAuto(autoCounterText, incrementPerSecond);
 });
 
 // Updates click amount on text
+const text = " Muscle Mass Gained!";
 function updateCounter(counterText: HTMLDivElement, count: number) {
-  totalCount = totalCount + count;
-  counterText.innerHTML = totalCount + " bicep curls completed";
+  totalCount += count;
+  counterText.innerHTML = totalCount + text;
 }
 
-// # to increment per second
-const incrementPerSecond: number = 1;
+const text2 = " muscle mass per second";
+let totalIncrement: number = 0;
+function updateAuto(autoText: HTMLDivElement, count: number) {
+  totalIncrement += count;
+  autoText.innerHTML = totalIncrement + text2;
+}
 
 // Per second counter text
 const autoCounterText = document.createElement("div");
 autoCounterText.style.fontSize = "15px";
-autoCounterText.innerHTML = incrementPerSecond + " curls per second";
+autoCounterText.innerHTML = totalIncrement + " muscle mass per second";
 
 autoCounter(incrementPerSecond);
 
-function autoCounter(cps: number) {
+function autoCounter(mmps: number) {
   let previousTime = performance.now();
   window.requestAnimationFrame(updateCounterPerFrame);
 
   function updateCounterPerFrame() {
-    const update = 1000 / cps;
+    const update = 1000 / mmps;
     if (performance.now() - previousTime > update) {
-      updateCounter(counterText, 1);
+      updateCounter(counterText, totalIncrement);
       previousTime = performance.now();
     }
     // console.log(previousTime);
     window.requestAnimationFrame(updateCounterPerFrame);
+    checkButton();
+  }
+}
+
+function checkButton() {
+  if (totalCount >= 10) {
+    sideButton.disabled = false;
+  } else {
+    sideButton.disabled = true;
   }
 }
 
 app.append(header);
 app.append(counterText);
 app.append(button);
+app.append(sideButton);
 app.append(autoCounterText);
